4691b0bfdb517775ca775d2a8ce48040c122002f
PandaPrince19 <sharmarajsekhar2.rs@gmail.com>
2021-12-23 22:55:07 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>
+          My <strong> ANIME </strong> Database
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+function Sidebar({ topAnime }) {
+  return (
+    <div>
+      <aside>
+        <nav>
+          <h3>Top Anime</h3>
+          {topAnime.map((anime) => (
+            <a
+              href={anime.url}
+              target='_blank'
+              key={anime.mal_id}
+              rel='noreferrer'
+            >
+              {anime.title}
+            </a>
+          ))}
+        </nav>
+      </aside>
+    </div>
+  );
+}
+
+export default Sidebar;
